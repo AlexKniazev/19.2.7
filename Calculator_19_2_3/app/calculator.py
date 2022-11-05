c56e64560a6d0169d5dca4c5864e19c0cc61a1dd
AlexKniazev <115535453+AlexKniazev@users.noreply.github.com>
2022-11-05 15:02:29 +0300

    Add files via upload

---
+++ Calculator_19_2_3/app/calculator.py
@@ -0,0 +1,15 @@
+
+
+class Calculator:
+#умножение
+    def multiply(self, x, y):
+        return x * y
+#деление
+    def division(self, x, y):
+        return x / y
+#вычитание
+    def subtraction(self, x, y):
+        return x - y
+#сложение
+    def adding(self, x, y):
+        return x + y
